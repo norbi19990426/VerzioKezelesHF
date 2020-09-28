054452f20532f910e6e30793f5f486ab52399ff7
Norbi <norbi19990426@gmail.com>
2020-09-28 15:47:43 +0200

    4.feladat

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -13,17 +13,41 @@ namespace VerzioKezelesHF
             double ossz = 0;
             Console.WriteLine("Hány számot szeretne megadni?: ");
             int hossz = Convert.ToInt32(Console.ReadLine());
+            int[] tomb = new int[hossz];
+            int max, min;
 
-            for (int i = 0; i < hossz; i++)
+            for (int i = 0; i < tomb.Length; i++)
             {
                 Console.WriteLine("Kérek {0} db számot: ",hossz);
                 int szamok = Convert.ToInt32(Console.ReadLine());
-                ossz += szamok;
+
+                tomb[i] = szamok;
             }
 
-            Console.WriteLine("A számok átlaga: {0}",ossz/hossz);
+            max = tomb[0];
+            min = tomb[0];
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+               
+                if (tomb[i] > max)
+                {
+                    max = tomb[i];
+                }
+                if (tomb[i] < min)
+                {
+                    min = tomb[i];
+                }
+
+                ossz += tomb[i];
+            }
+
+            Console.WriteLine("A számok átlaga: {0} \n" +
+                "Legnagyobb szám: {1} \n" +
+                "Legkisebb szám: {2}",ossz/hossz, max, min);
 
 
         }
     }
 }
+
