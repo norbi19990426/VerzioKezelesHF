c86e120235d3022977fd1742fbd477f8d41bf15c
Norbi <norbi19990426@gmail.com>
2020-09-28 15:24:41 +0200

    2.feladat

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -10,6 +10,18 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
+            double ossz = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Kérek 10 db számot: ");
+                int szamok = Convert.ToInt32(Console.ReadLine());
+                ossz += szamok;
+            }
+
+            Console.WriteLine("A számok átlaga: {0}",ossz/10);
+
+
         }
     }
 }
